3f73a5c92dc9caa251d8937590f64fe3275a05f5
gifhary <gifhary.1997@gmail.com>
2021-12-26 21:43:49 +0700

    few error adjustments

--- lib/widget/ini_text_field.dart
+++ lib/widget/ini_text_field.dart
@@ -18,32 +18,30 @@ class IniTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Flexible(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 9),
-            child: Text(label ?? ""),
-          ),
-          TextField(
-            readOnly: readOnly ?? false,
-            obscureText: obscureText ?? false,
-            controller: controller,
-            decoration: InputDecoration(
-              focusedBorder: const OutlineInputBorder(
-                borderSide: BorderSide(
-                    color: Color.fromRGBO(117, 111, 99, 1), width: 1),
-              ),
-              enabledBorder: const OutlineInputBorder(
-                borderSide: BorderSide(
-                    color: Color.fromRGBO(117, 111, 99, 1), width: 1),
-              ),
-              hintText: hintText,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 9),
+          child: Text(label ?? ""),
+        ),
+        TextField(
+          readOnly: readOnly ?? false,
+          obscureText: obscureText ?? false,
+          controller: controller,
+          decoration: InputDecoration(
+            focusedBorder: const OutlineInputBorder(
+              borderSide:
+                  BorderSide(color: Color.fromRGBO(117, 111, 99, 1), width: 1),
             ),
+            enabledBorder: const OutlineInputBorder(
+              borderSide:
+                  BorderSide(color: Color.fromRGBO(117, 111, 99, 1), width: 1),
+            ),
+            hintText: hintText,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
